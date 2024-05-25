93debad5d781c7e70110abb12d3ff29290af2f37
AmiasYaska <amiasyaska@gmail.com>
2024-05-25 23:19:31 +0300

    changes

--- Gemfile
+++ Gemfile
@@ -44,6 +44,8 @@ gem "bootsnap", require: false
 
 # MY CUSTOM GEMS
 gem "devise"
+gem 'friendly_id', '~> 5.5.0'
+
 
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   resources :trees
   get 'home/index'
 
-  get 'home/pricing'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
